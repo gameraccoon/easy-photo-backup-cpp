f01ed4dcea4b13b4d18ce435350e79fdd4505991
Pavel Grebnev <ruwhynot@gmail.com>
2026-01-11 15:48:23 +0100

    Added a bash script to reformat C++ code only in the related directories

---
+++ tools/validate/reformat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+pushd $parent_path > /dev/null
+	python run-clang-format.py ../../CommonShared ../../ClientShared ../../ServerShared ../../ClientCli ../../ServerCli
+popd > /dev/null
