427379810a590437038e227648d99be0a6e49b52
Pavel Grebnev <ruwhynot@gmail.com>
2026-01-18 10:16:20 +0100

    Add shared C++ libs to the android app

--- platforms/android/app/src/main/cpp/ffi_bindings.cpp
+++ platforms/android/app/src/main/cpp/ffi_bindings.cpp
@@ -2,11 +2,13 @@
 #include <string>
 #include <format>
 
+#include "client_shared/example/example.h"
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_unnamed_easyphotobackup_MainActivity_stringFromJNI(
 	JNIEnv* env,
 	jobject /* this */)
 {
-	std::string hello = std::format("Hello from C++ {}", 10);
+	std::string hello = std::format("Hello from C++ {}", example::EXAMPLE_CLIENT_VALUE);
 	return env->NewStringUTF(hello.c_str());
 }
