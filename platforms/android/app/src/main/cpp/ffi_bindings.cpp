687088bfcbd5768e9197740c4ece8b7e557cbe54
Pavel Grebnev <ruwhynot@gmail.com>
2026-01-18 10:33:02 +0100

    Reformat the android source file

--- platforms/android/app/src/main/cpp/ffi_bindings.cpp
+++ platforms/android/app/src/main/cpp/ffi_bindings.cpp
@@ -1,13 +1,15 @@
-#include <jni.h>
-#include <string>
 #include <format>
+#include <string>
+
+#include <jni.h>
 
 #include "client_shared/example/example.h"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_unnamed_easyphotobackup_MainActivity_stringFromJNI(
 	JNIEnv* env,
-	jobject /* this */)
+	jobject /* this */
+)
 {
 	std::string hello = std::format("Hello from C++ {}", example::EXAMPLE_CLIENT_VALUE);
 	return env->NewStringUTF(hello.c_str());
--- tools/validate/reformat.sh
+++ tools/validate/reformat.sh
@@ -3,5 +3,5 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 pushd $parent_path > /dev/null
-	python run-clang-format.py ../../CommonShared ../../ClientShared ../../ServerShared ../../ClientCli ../../ServerCli
+	python run-clang-format.py ../../CommonShared ../../ClientShared ../../ServerShared ../../ClientCli ../../ServerCli ../../platforms/android/app/src/main/cpp
 popd > /dev/null
