349779f4de54cd58df8a6091b5d0a477361d0652
Pavel Grebnev <ruwhynot@gmail.com>
2026-03-06 23:16:55 +0100

    Remove usage of the example code

--- platforms/android/app/src/main/cpp/ffi_bindings.cpp
+++ platforms/android/app/src/main/cpp/ffi_bindings.cpp
@@ -3,14 +3,12 @@
 
 #include <jni.h>
 
-#include "client_shared/example/example.h"
-
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_unnamed_easyphotobackup_MainActivity_stringFromJNI(
 	JNIEnv* env,
 	jobject /* this */
 )
 {
-	std::string hello = std::format("Hello from C++ {}", example::EXAMPLE_CLIENT_VALUE);
+	std::string hello = std::format("Hello from C++ {}", 20);
 	return env->NewStringUTF(hello.c_str());
 }
