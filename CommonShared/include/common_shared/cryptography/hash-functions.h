53a196b2d9b96d945e0d081c8ab5338cbb218865
Pavel Grebnev <ruwhynot@gmail.com>
2026-03-28 13:44:59 +0100

    Added unit-tests for HMAC_blake2b function

--- CommonShared/include/common_shared/cryptography/hash-functions.h
+++ CommonShared/include/common_shared/cryptography/hash-functions.h
@@ -15,6 +15,11 @@ namespace Cryptography
 
 	using HashResult = ByteSequence<Tag::HashResult, HASHLEN>;
 
+#ifdef WITH_TESTS
+	// it is only used for the HKDF implementations, but we still want to test it
+	void HMAC_blake2b(const HashResult& key, std::span<const uint8_t> data, HashResult& outMac);
+#endif
+
 	void HKDF_blake2b(
 		const HashResult& chainingKey,
 		std::span<const uint8_t> inputKeyMaterial,
