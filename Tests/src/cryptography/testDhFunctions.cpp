a514472e2e0e22b591e5fd79585c6022112ba89a
Pavel Grebnev <ruwhynot@gmail.com>
2026-03-28 18:22:56 +0100

    Added a simple test for Diffie-Hellman functions

---
+++ Tests/src/cryptography/testDhFunctions.cpp
@@ -0,0 +1,16 @@
+// Copyright (C) Pavel Grebnev 2026
+// Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+
+#include <gtest/gtest.h>
+
+#include "common_shared/cryptography/dh-functions.h"
+
+TEST(CryptographyDhFunctions, DhX25519_test)
+{
+	Cryptography::Keypair keys1 = Cryptography::generateKeypair_x25519();
+	Cryptography::Keypair keys2 = Cryptography::generateKeypair_x25519();
+	Cryptography::DhResult dhResult1 = Cryptography::diffieHellman_x25519(keys1.secretKey, keys2.publicKey);
+	Cryptography::DhResult dhResult2 = Cryptography::diffieHellman_x25519(keys2.secretKey, keys1.publicKey);
+
+	EXPECT_EQ(dhResult1.raw, dhResult2.raw);
+}
