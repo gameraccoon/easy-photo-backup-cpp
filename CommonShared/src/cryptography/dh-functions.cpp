c022a7e9882f148351f19228da1f96a40be8920c
Pavel Grebnev <ruwhynot@gmail.com>
2026-03-29 11:32:00 +0200

    Make sure all monocypher calls that require arguments of specific length have assertions right before the calls

--- CommonShared/src/cryptography/dh-functions.cpp
+++ CommonShared/src/cryptography/dh-functions.cpp
@@ -17,8 +17,8 @@ namespace Cryptography
 
 		static_assert(newKeypair.publicKey.raw.size() == 32, "The crypto_x25519_public_key implementation supports only 32 byte keys");
 		static_assert(newKeypair.secretKey.raw.size() == 32, "The crypto_x25519_public_key implementation supports only 32 byte keys");
-
 		crypto_x25519_public_key(newKeypair.publicKey.raw.data(), newKeypair.secretKey.raw.data());
+
 		return newKeypair;
 	}
 
@@ -29,10 +29,9 @@ namespace Cryptography
 		// static_assert doesn't work on constexpr functions on arguments for some reason, do sizeof to have at least something
 		static_assert(sizeof(publicKey.raw) == 32, "The crypto_x25519 implementation supports only 32 byte keys");
 		static_assert(sizeof(secretKey.raw) == 32, "The crypto_x25519 implementation supports only 32 byte keys");
-		static_assert(sizeof(publicKey.raw) == 32, "The crypto_x25519 implementation supports only 32 byte keys");
 		static_assert(result.raw.size() == 32, "The crypto_x25519 implementation supports only 32 byte keys");
-
 		crypto_x25519(result.raw.data(), secretKey.raw.data(), publicKey.raw.data());
+
 		return result;
 	}
 } // namespace Cryptography
