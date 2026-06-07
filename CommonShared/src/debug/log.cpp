25e4855cfb1b84b615d5043af97c9e0e3bb31ea2
Pavel Grebnev <ruwhynot@gmail.com>
2026-06-07 18:07:34 +0200

    Added debug logging implementation for Android

--- CommonShared/src/debug/log.cpp
+++ CommonShared/src/debug/log.cpp
@@ -3,8 +3,13 @@
 
 #include "common_shared/debug/log.h"
 
-#include <iostream>
 #include <mutex>
+
+#ifdef __ANDROID_API__
+#include <android/log.h>
+#else
+#include <iostream>
+#endif
 
 namespace Debug::Log
 {
@@ -21,9 +26,16 @@ namespace Debug::Log
 	{
 		std::lock_guard<std::mutex> lock(Internal::getDebugLogMutex());
 
+#ifdef __ANDROID_API__
+        // we can't know if string_view contains null terminator or not, so we have to add it
+        // we should add a custom string_view type
+        const std::string textStr(text);
+        __android_log_print(ANDROID_LOG_DEBUG, "native log", "%s", textStr.c_str());
+#else
 		std::cout
 			<< text
 			<< '\n'
 			<< std::flush;
+#endif
 	}
 } // namespace Debug::Log
